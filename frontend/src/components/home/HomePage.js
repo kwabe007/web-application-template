c04423a0b86560c40cd786ffdc51bbe25a9173ba
Melany <melanycristyg@gmail.com>
2019-11-19 15:52:40 +0100

    Delete unnecesary code

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -7,13 +7,6 @@ class HomePage extends Component {
                 <div className="card-body">
                     <h4 className="card-title">Musing</h4>
                     <p>Your place to write your concert experience</p>
-                    <ul>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                    </ul>
                 </div>
             </div>
         );
