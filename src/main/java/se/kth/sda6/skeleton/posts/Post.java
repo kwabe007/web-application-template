b1ef0ec948da6b8d623055c226e6bc30df014200
Asmaa-Abdullah <asmaa_abdullah71@yahoo.com>
2019-11-20 00:56:44 +0100

    Add a post table as well as the process of database handling of generating the id.

--- src/main/java/se/kth/sda6/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda6/skeleton/posts/Post.java
@@ -1,19 +1,34 @@
 package se.kth.sda6.skeleton.posts;
 
+import javax.persistence.*;
+
 // @TODO add Hibernate annotations to define which table and columns should be used to save the Post Object.
+@Entity
+@Table(name = "post")
 public class Post {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // The DB will handle the process of generating the id.
     private Long id;
+    @Column(name = "bandName")
+    private String bandName;
+    @Column(name = "tourName")
+    private String tourName;
+    @Column(name = "location")
+    private String location;
+    @Column(name = "date")
+    private String date;
 
-    private String body;
-
-//    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
-//    private List<Comment> comments = new ArrayList<>();
 
     public Post() {
     }
 
-    public Post(String body) {
-        this.body = body;
+    public Post(Long id, String bandName, String tourName, String location, String date) {
+        this.id = id;
+        this.bandName = bandName;
+        this.tourName = tourName;
+        this.location = location;
+        this.date = date;
     }
 
     public Long getId() {
@@ -24,21 +39,35 @@ public class Post {
         this.id = id;
     }
 
-    public String getBody() {
-        return body;
+    public String getBandName() {
+        return bandName;
     }
 
-    public void setBody(String body) {
-        this.body = body;
+    public void setBandName(String bandName) {
+        this.bandName = bandName;
     }
 
-//    public void addComment(Comment comment) {
-//        comments.add( comment );
-//        comment.setPost( this );
-//    }
-//
-//    public void removeComment(Comment comment) {
-//        comments.remove( comment );
-//        comment.setPost( null );
-//    }
+    public String getTourName() {
+        return tourName;
+    }
+
+    public void setTourName(String tourName) {
+        this.tourName = tourName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
