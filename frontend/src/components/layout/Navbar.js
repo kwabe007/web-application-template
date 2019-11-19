e103e7854921a38af9323e7ba7a1de9282ae31bb
Melany <melanycristyg@gmail.com>
2019-11-19 14:07:26 +0100

    Add new branding and logo

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Navbar({onLogout}) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">SDA 6 starter</a>
+            <a className="navbar-brand" id="logo" href="./home/HomePage.js"><img src="../../src/public/logo.jpg" alt="logo"></img></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
