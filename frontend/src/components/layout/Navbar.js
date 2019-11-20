b9f65d4bce41ade7de45f9c3b24cf19f7390fd28
basiazorychta <basiazorychta@gmail.com>
2019-11-20 13:43:52 +0100

    Minor changing in H2 in Navbar , add padding

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -5,11 +5,11 @@ function Navbar({onLogout}) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <img class="navlogo" alt="No2ProLogo" src="logo192.png" width="100" height="100" class="d-inline-block align-top" alt=""/>
-            <a className="navbar-brand" href="#">
-            <h2> &nbsp;&nbsp; From Nogrammer &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </h2>
-            <h2> &nbsp;&nbsp; to Programmer &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </h2>
+            <a className= "col px-md-4 navbar-brand" href="#">
+            <h2>  From Nogrammer </h2>
+            <h2>  to Programmer  </h2>
             </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="col px-md-10 navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarColor01">
