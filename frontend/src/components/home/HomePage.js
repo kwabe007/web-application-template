fc731b6a253781c580e6c9fb1ca60ae37c04ca3a
jshortz <jessica.shortz@gmail.com>
2019-11-20 13:33:40 +0100

    Added some more flavorful text and edited existing text in LoginPage, HomePage, and PostForm

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -5,8 +5,9 @@ class HomePage extends Component {
         return (
             <div className="card">
                 <div className="card-body">
-                    <h4 className="card-title">SDA 6 starter template</h4>
-                    <p>This starter template is based on Spring, PostgreSQL, React, React router and Axios. Check the following links for documentation and guides:</p>
+                    <h4 className="card-title">Welcome to Nogrammer to Programmer!</h4>
+                    <p>We are a group of students in Stockholm fighting off the winter cold by learning programming. We built this site
+                     with React, Spring, CSS, JavaScript, Java, Hibernate, and some other goodies. Here are some links:</p>
                     <ul>
                         <li><a href="https://spring.io/projects/spring-boot">Spring</a></li>
                         <li><a href="https://www.postgresql.org">PostgreSQL</a></li>
