a1d44788d79919ad8e323d087ac7f7728aaa6608
jshortz <jessica.shortz@gmail.com>
2019-11-21 10:56:44 +0100

    Update HomePage.js
    Change the HomePage.js text to the No to Pro text (rather than the SDA6 default text). Changed links in HomePage.js to open in a new window.

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -5,14 +5,14 @@ class HomePage extends Component {
         return (
                     <div className="card">
                         <div className="card-body">
-                            <h4 className="card-title">SDA 6 starter template</h4>
-                            <p>This starter template is based on Spring, PostgreSQL, React, React router and Axios. Check the following links for documentation and guides:</p>
+                            <h4 className="card-title">Welcome to Nogrammer to Programmer!</h4>
+                            <p>We are a group of students in Stockholm fighting off the winter cold by learning how to program. We built this site with Spring, PostgreSQL, React, React router, Axios, and other goodies. Check the following links for documentation and guides:</p>
                             <ul>
-                                <li><a href="https://spring.io/projects/spring-boot">Spring</a></li>
-                                <li><a href="https://www.postgresql.org">PostgreSQL</a></li>
-                                <li><a href="https://reactjs.org">React</a></li>
-                                <li><a href="https://reacttraining.com/react-router/web/guides/quick-start">React Router</a></li>
-                                <li><a href="https://github.com/axios/axios">Axios</a></li>
+                                <li><a href="https://spring.io/projects/spring-boot" target="_blank">Spring</a></li>
+                                <li><a href="https://www.postgresql.org" target="_blank">PostgreSQL</a></li>
+                                <li><a href="https://reactjs.org" target="_blank">React</a></li>
+                                <li><a href="https://reacttraining.com/react-router/web/guides/quick-start" target="_blank">React Router</a></li>
+                                <li><a href="https://github.com/axios/axios" target="_blank">Axios</a></li>
                             </ul>
                         </div>
                     </div>
